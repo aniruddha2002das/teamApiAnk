24ff821ece791114ed2e450c27a1df4839225c5b
user-ank <ankush1maji@gmail.com>
2023-01-19 15:42:31 +0530

    Added post component by Anuj

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import "./Post.css"
+// import { useStateValue } from '../../StateProvider'
 
-function Post() {
-  return (
-    <div>
-      
-    </div>
-  )
+
+function Post({ id, title, description, writtenby, posttime, noOfLike, noOfDislike }) {
+
+
+	return (
+		<div className='Posts'>
+			<div className="Posts_info">
+				<p><h3>{title}</h3></p>
+				<p className="Posts_description">{description}</p>
+				<div className="post_stat">
+					<div className="post_writtenby details">{writtenby}</div>
+					<div className="post_time details">{posttime}</div>
+					<div className="noOfLike details">{noOfLike}</div>
+					<div className="noOfDislike details">{noOfDislike}</div>
+				</div>
+			</div>
+			<button className='like'>Like</button>
+			<button className='dislike'>Dislike</button>
+
+		</div>
+	)
 }
 
 export default Post
+
+// Done by Anuj Patel
